3a7f7206d3d1474a6d2e60949a793d2ffe044437
Vishrut Shah <vissha@microsoft.com>
2017-06-21 16:00:55 -0400

    Base version of the nodeJs cli

---
+++ cli.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+'use strict';
+
+var yargs = require('yargs'),
+  os = require('os'),
+  log = require('./lib/util/logging');
+
+var defaultLogDir = log.directory;
+var logFilepath = log.filepath;
+var packageVersion = require('./package.json').version;
+
+yargs
+  .version(packageVersion)
+  .commandDir('lib/commands')
+  .option('h', { alias: 'help' })
+  .option('l', {
+    alias: 'logLevel',
+    describe: 'Set the logging level for console.',
+    choices: ['off', 'json', 'error', 'warn', 'info', 'verbose', 'debug', 'silly'],
+    default: 'warn'
+  })
+  .option('f', {
+    alias: 'logFilepath',
+    describe: `Set the log file path. It must be an absolute filepath. ` +
+    `By default the logs will stored in a timestamp based log file at "${defaultLogDir}".`
+  })
+  .global(['h', 'l', 'f'])
+  .help()
+  .argv;
+
+if (yargs.argv._.length === 0 && yargs.argv.h === false) {
+  yargs.coerce('help', function (arg) { return true; }).argv;
+}
