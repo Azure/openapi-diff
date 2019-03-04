b8227ac35f94d26931bf7db10ea6f13d423e062a
Sergey Shandar <sergey.shandar@gmail.com>
2019-03-04 15:16:55 -0800

    Parser update

--- openapi-diff/src/modeler/AutoRest.Swagger/SwaggerParser.cs
+++ openapi-diff/src/modeler/AutoRest.Swagger/SwaggerParser.cs
@@ -10,35 +10,13 @@ namespace AutoRest.Swagger
 {
     public static class SwaggerParser
     {
-        class ObjectConverter : JsonConverter
-        {
-            public override bool CanConvert(Type objectType) => false;
-
-            public override object ReadJson(
-                JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer
-            ) => existingValue;
-
-            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-            {
-            }
-        }
-
         public static ParsedJson<ServiceDefinition> Parse(string swaggerDocument)
         {
             var raw = JToken.Parse(swaggerDocument);
-            var converters = new JsonConverterCollection { };
-            converters.Add(new ObjectConverter());
-            return raw.ToParsedJson(raw.ToObject<ServiceDefinition>(JsonSerializer.Create(new JsonSerializerSettings
+            return raw.ToParsedJson(raw.ToObject<ServiceDefinition>(new JsonSerializer 
             {
-                Converters = converters,
                 MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
-            })));
-            /*
-            return JsonConvert.DeserializeObject<ServiceDefinition>(swaggerDocument, new JsonSerializerSettings
-            {
-                MetadataPropertyHandling = MetadataPropertyHandling.Ignore
-            });
-            */
+            }));
         }
     }
 }
