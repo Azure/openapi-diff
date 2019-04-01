0d35e8c345c983d3f55b67774eaaee6198a53a90
Sergey Shandar <sergey.shandar@gmail.com>
2019-04-01 13:58:28 -0700

    test readme file and tags.

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,6 +1,7 @@
 import * as index from "../index"
 import * as assert from "assert"
 import * as path from "path"
+import { compareTags } from '../lib/validate';
 
 jest.setTimeout(10000)
 
@@ -41,6 +42,50 @@ describe("index", () => {
     const result = JSON.parse(resultStr)
     const newFilePath = "file:///" + path.resolve(newFile).split("\\").join("/")
     const oldFilePath = "file:///" + path.resolve(oldFile).split("\\").join("/")
+    const expected = [
+      {
+        code: "NoVersionChange",
+        docurl: "https://github.com/Azure/openapi-diff/tree/master/docs/rules/1001.md",
+        id: "1001",
+        message: "The versions have not changed.",
+        mode: "Update",
+        new: {
+          ref: `${newFilePath}#`,
+          location: `${newFilePath}:1:1`,
+          path: ""
+        },
+        old: {
+          ref: `${oldFilePath}#`,
+          location: `${oldFilePath}:1:1`,
+          path: ""
+        },
+        type: "Info"
+      },
+      {
+        code: "AddedPath",
+        docurl: "https://github.com/Azure/openapi-diff/tree/master/docs/rules/1038.md",
+        id: "1038",
+        message: "The new version is adding a path that was not found in the old version.",
+        mode: "Addition",
+        new: {
+          location: `${newFilePath}:8:5`,
+          path: "paths./x",
+          ref: `${newFilePath}#/paths/~1x`
+        },
+        old: {},
+        type: "Info"
+      }
+    ]
+    assert.deepStrictEqual(result, expected)
+  })
+  it("full", async () => {
+    const oldFile = "src/test/full/old/readme.md"
+    const newFile = "src/test/full/new/readme.md"
+    const diff = new index.OpenApiDiff({})
+    const resultStr = await diff.compare(oldFile, newFile, "2019", "2019")
+    const result = JSON.parse(resultStr)
+    const newFilePath = "file:///" + path.resolve("src/test/full/new/openapi.json").split("\\").join("/")
+    const oldFilePath = "file:///" + path.resolve("src/test/full/old/openapi.json").split("\\").join("/")
     const expected = [
       {
         code: "NoVersionChange",
