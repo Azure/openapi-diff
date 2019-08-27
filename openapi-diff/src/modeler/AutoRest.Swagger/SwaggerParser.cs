e897816066e6c0e1c397da9e7c2af44d73686168
Zhiqing Qiu <michaeljqzq@users.noreply.github.com>
2019-08-27 13:41:32 +0800

    Fix common parameter issue (#150) (#152)

--- openapi-diff/src/modeler/AutoRest.Swagger/SwaggerParser.cs
+++ openapi-diff/src/modeler/AutoRest.Swagger/SwaggerParser.cs
@@ -13,6 +13,7 @@ namespace AutoRest.Swagger
     {
         public static JsonDocument<ServiceDefinition> Parse(string swaggerDocument, string fileName)
         {
+            swaggerDocument = extractCommonParameters(swaggerDocument);
             var raw = JToken.Parse(swaggerDocument);
             return raw.ToJsonDocument(
                 raw.ToObject<ServiceDefinition>(new JsonSerializer 
@@ -21,6 +22,83 @@ namespace AutoRest.Swagger
                 }),
                 fileName
             );
+        }
+
+        // To extract the common parameters directly defined under {path}, and merge it with all operations parameters
+        private static string extractCommonParameters(string json)
+        {
+            JObject obj = JObject.Parse(json);
+            var paths = (JObject) obj["paths"];
+            const string PATH_PARAMETERS_KEY = "parameters";
+            bool changed = false;
+            if(paths != null)
+            {
+                foreach (var path in paths)
+                {
+                    if (path.Value[PATH_PARAMETERS_KEY] != null)
+                    {
+                        changed = true;
+                        var paras = path.Value.Value<JArray>(PATH_PARAMETERS_KEY);
+                        foreach (var operation in (JObject)path.Value)
+                        {
+                            if (operation.Key == PATH_PARAMETERS_KEY) continue;
+                            if (operation.Value[PATH_PARAMETERS_KEY] == null)
+                            {
+                                operation.Value[PATH_PARAMETERS_KEY] = new JArray();
+                            }
+                            foreach (var para in paras)
+                            {
+                                // Add common parameters to all operations only when they are not already defined in operations
+                                var operationParaArray = ((JArray)operation.Value[PATH_PARAMETERS_KEY]);
+                                if (para["name"] != null)
+                                {
+                                    string name = para.Value<string>("name");
+
+                                    bool alreadyExist = false;
+
+                                    foreach (var p in operationParaArray)
+                                    {
+                                        if (p["name"] != null && p.Value<string>("name") == name)
+                                        {
+                                            alreadyExist = true;
+                                            break;
+                                        }
+                                    }
+
+                                    if (!alreadyExist)
+                                    {
+                                        operationParaArray.Add(para);
+                                    }
+                                }
+                                else if (para["$ref"] != null)
+                                {
+                                    string reference = para.Value<string>("$ref");
+
+                                    bool alreadyExist = false;
+
+                                    foreach (var p in operationParaArray)
+                                    {
+                                        if (p["$ref"] != null && p.Value<string>("name") == reference)
+                                        {
+                                            alreadyExist = true;
+                                            break;
+                                        }
+                                    }
+
+                                    if (!alreadyExist)
+                                    {
+                                        operationParaArray.Add(para);
+                                    }
+                                }
+
+                            }
+                        }
+                        ((JObject)path.Value).Remove(PATH_PARAMETERS_KEY);
+                    }
+                }
+            }
+            
+            return changed ? obj.ToString() : json;
         }
     }
 }
