821f26c45db76c70e4469144ff4bc868dc83dc62
Mike Kistler <mikekistler@microsoft.com>
2021-07-05 10:39:49 -0700

    test: add test for RequestBodyFormatNoLongerSupported

--- src/test/test.ts
+++ src/test/test.ts
@@ -626,4 +626,34 @@ describe("index", () => {
     ]
     assert.deepStrictEqual(result, expected)
   })
+  it("flags 1003 - RequestBodyFormatNoLongerSupported", async () => {
+    const diff = new index.OpenApiDiff({})
+    const oldFile = "src/test/rule-1003/case1.old.json"
+    const newFile = "src/test/rule-1003/case1.new.json"
+    const resultStr = await diff.compare(oldFile, newFile)
+    const result = JSON.parse(resultStr)
+    const newFilePath = fileUrl(path.resolve(newFile))
+    const oldFilePath = fileUrl(path.resolve(oldFile))
+    const expected = [
+      {
+        id: "1003",
+        code: "RequestBodyFormatNoLongerSupported",
+        message: "The new version does not support 'text/plain' as a request body format.",
+        mode: "Removal",
+        old: {
+          ref: `${oldFilePath}#/consumes`,
+          path: "paths./pets.post.consumes",
+          location: `${oldFilePath}:7:3`
+        },
+        new: {
+          ref: `${newFilePath}#/consumes`,
+          path: "paths./pets.post.consumes",
+          location: `${newFilePath}:7:3`
+        },
+        type: "Warning",
+        docUrl: "https://github.com/Azure/openapi-diff/tree/master/docs/rules/1003.md"
+      }
+    ]
+    assert.deepStrictEqual(result, expected)
+  })
 })
