9a1cc8e57c4362377a9b02c5620feba3895e3560
jianye xi <jianyxi@microsoft.com>
2022-09-29 17:24:17 +0800

    bugfix fo xmsEnumChange

--- openapi-diff/src/modeler/AutoRest.Swagger/Model/XmsEnumExtension.cs
+++ openapi-diff/src/modeler/AutoRest.Swagger/Model/XmsEnumExtension.cs
@@ -1,8 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace AutoRest.Swagger.Model
 {
@@ -22,7 +24,9 @@ namespace AutoRest.Swagger.Model
     public class XmsEnumExtension 
     {
         public string Name { get; set; }
-        
+
+        [DefaultValue(true)]
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
         public Boolean ModelAsString { get; set; }
 
         public IList<XmsEnumValue> values { get; set; }
