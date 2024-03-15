16d75fea47a7d69a8f5e9bd8f7b78f37609d4957
Mike Harder <mharder@microsoft.com>
2024-03-15 07:18:07 +0000

    prettier

--- src/test/requiredPropertyNameXMsEnumTest.ts
+++ src/test/requiredPropertyNameXMsEnumTest.ts
@@ -5,7 +5,7 @@ test("'Missing required property: name' at x-ms-enum", async () => {
   const diff = new index.OpenApiDiff({})
   const resultStr = await diff.compare(
     "src/test/required-property-name-x-ms-enum/source/openapi.json",
-    "src/test/required-property-name-x-ms-enum/target/openapi.json",
+    "src/test/required-property-name-x-ms-enum/target/openapi.json"
   )
   const result: index.Messages = JSON.parse(resultStr)
 })
