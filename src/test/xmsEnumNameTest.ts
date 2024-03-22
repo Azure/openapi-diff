1fea6f038fc855ca58c5f6bcb30edbacf8283d82
Mike Harder <mharder@microsoft.com>
2024-03-22 12:20:46 -0700

    Fix 'Missing required property: name' at x-ms-enum (#302)
    - Depends on @microsoft.azure/autorest-core@2.0.4424

---
+++ src/test/xmsEnumNameTest.ts
@@ -0,0 +1,53 @@
+import * as assert from "assert"
+import * as path from "path"
+import * as index from "../index"
+import { fileUrl } from "./fileUrl"
+
+test("xms-enum-name", async () => {
+  const diff = new index.OpenApiDiff({})
+  const oldFile = "src/test/xms-enum-name/old.json"
+  const newFile = "src/test/xms-enum-name/new.json"
+  const resultStr = await diff.compare(oldFile, newFile)
+  const result = JSON.parse(resultStr)
+  const newFilePath = fileUrl(path.resolve(newFile))
+  const oldFilePath = fileUrl(path.resolve(oldFile))
+  const expected = [
+    {
+      code: "NoVersionChange",
+      docUrl: "https://github.com/Azure/openapi-diff/tree/master/docs/rules/1001.md",
+      id: "1001",
+      message: "The versions have not changed.",
+      mode: "Update",
+      new: {
+        ref: `${newFilePath}#`,
+        path: "",
+        location: `${newFilePath}:1:1`
+      },
+      old: {
+        ref: `${oldFilePath}#`,
+        path: "",
+        location: `${oldFilePath}:1:1`
+      },
+      type: "Info"
+    },
+    {
+      code: "XmsEnumChanged",
+      docUrl: "https://github.com/Azure/openapi-diff/tree/master/docs/rules/1047.md",
+      id: "1047",
+      message: "The new version has a different x-ms-enum 'name' than the previous one.",
+      mode: "Update",
+      new: {
+        ref: `${newFilePath}#/definitions/Foo/properties/bar`,
+        path: "definitions.Foo.properties.bar",
+        location: `${newFilePath}:13:9`
+      },
+      old: {
+        ref: `${oldFilePath}#/definitions/Foo/properties/bar`,
+        path: "definitions.Foo.properties.bar",
+        location: `${oldFilePath}:13:9`
+      },
+      type: "Error"
+    }
+  ]
+  assert.deepStrictEqual(result, expected)
+})
