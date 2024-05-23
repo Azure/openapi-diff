96163e815f8472cad2d2dad3c3b757df166bb0dc
Mike Harder <mharder@microsoft.com>
2024-05-23 05:31:51 +0000

    Add test for compatible properties with a $ref

--- src/test/compatiblePropertiesTest.ts
+++ src/test/compatiblePropertiesTest.ts
@@ -35,3 +35,36 @@ test("compatible-properties", async () => {
   ]
   assert.deepStrictEqual(result, expected)
 })
+
+// Ensures that $ref are resolved when determining if types are compatible.
+// For example, these should be compatible:
+// 1. "bar": { "type":"string" }
+// 2. "bar": { "$ref":"#/definitions/MyString" }, "MyString": { "type": "string" }
+test("compatible-properties-ref", async () => {
+  const diff = new OpenApiDiff({})
+  const file = "src/test/specs/compatible-properties-ref.json"
+  const resultStr = await diff.compare(file, file)
+  const result = JSON.parse(resultStr)
+  const filePath = fileUrl(path.resolve(file))
+  const expected = [
+    {
+      code: "NoVersionChange",
+      docUrl: "https://github.com/Azure/openapi-diff/tree/master/docs/rules/1001.md",
+      id: "1001",
+      message: "The versions have not changed.",
+      mode: "Update",
+      new: {
+        ref: `${filePath}#`,
+        path: "",
+        location: `${filePath}:1:1`
+      },
+      old: {
+        ref: `${filePath}#`,
+        path: "",
+        location: `${filePath}:1:1`
+      },
+      type: "Info"
+    }
+  ]
+  assert.deepStrictEqual(result, expected)
+})
