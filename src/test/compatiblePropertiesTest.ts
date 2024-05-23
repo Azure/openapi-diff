1cb7f55f6e53baacb8a0a2108e140748e23e3e04
Mike Harder <mharder@microsoft.com>
2024-05-23 18:51:18 +0000

    Improve tests

--- src/test/compatiblePropertiesTest.ts
+++ src/test/compatiblePropertiesTest.ts
@@ -7,42 +7,14 @@ import { fileUrl } from "./fileUrl"
 // Given a property with given type and name
 // When another property with the same name and compatible type is referenced
 // Then no issue is reported, as this is a valid scenario
-test("compatible-properties", async () => {
-  const diff = new OpenApiDiff({})
-  const file = "src/test/specs/compatible-properties.json"
-  const resultStr = await diff.compare(file, file)
-  const result = JSON.parse(resultStr)
-  const filePath = fileUrl(path.resolve(file))
-  const expected = [
-    {
-      code: "NoVersionChange",
-      docUrl: "https://github.com/Azure/openapi-diff/tree/master/docs/rules/1001.md",
-      id: "1001",
-      message: "The versions have not changed.",
-      mode: "Update",
-      new: {
-        ref: `${filePath}#`,
-        path: "",
-        location: `${filePath}:1:1`
-      },
-      old: {
-        ref: `${filePath}#`,
-        path: "",
-        location: `${filePath}:1:1`
-      },
-      type: "Info"
-    }
-  ]
-  assert.deepStrictEqual(result, expected)
-})
-
-// Ensures that $ref are resolved when determining if types are compatible.
+//
+// Also ensures that $ref are resolved when determining if types are compatible.
 // For example, these should be compatible:
 // 1. "bar": { "type":"string" }
 // 2. "bar": { "$ref":"#/definitions/MyString" }, "MyString": { "type": "string" }
-test("compatible-properties-ref", async () => {
+test("compatible-properties", async () => {
   const diff = new OpenApiDiff({})
-  const file = "src/test/specs/compatible-properties-ref.json"
+  const file = "src/test/specs/compatible-properties.json"
   const resultStr = await diff.compare(file, file)
   const result = JSON.parse(resultStr)
   const filePath = fileUrl(path.resolve(file))
--- src/test/incompatiblePropertiesTest.ts
+++ src/test/incompatiblePropertiesTest.ts
@@ -7,9 +7,16 @@ import { fileUrl } from "./fileUrl"
 // Given a property with given type and name
 // When another property with the same name but an incompatible type is referenced
 // Then an issue is reported, with output providing the exact source file locations of both of the occurrences of the property.
-test("incompatible-properties", async () => {
+//
+// Also ensures that $ref are resolved when determining if types are compatible.
+// For example, these should be incompatible:
+// 1. "bar": { "type":"string" }
+// 2. "bar": { "$ref":"#/definitions/MyObject" }, "MyObject": { "type": "object" }
+test.each(
+  ["string-object", "string-refobject", "refstring-object", "refstring-refobject"]
+)("incompatible-properties-%s", async (prop) => {
   const diff = new OpenApiDiff({})
-  const file = "src/test/specs/incompatible-properties.json"
+  const file = `src/test/specs/incompatible-properties/${prop}.json`
   const filePath = fileUrl(path.resolve(file))
 
   try {
@@ -19,11 +26,74 @@ test("incompatible-properties", async () => {
     const e = error as Error
     assert.equal(
       e.message,
-      "incompatible properties : bar\n" +
-        "  definitions/FooBarString/properties/bar\n" +
-        `    at ${filePath}#L13:8\n` +
-        "  definitions/FooBarObject/properties/bar\n" +
-        `    at ${filePath}#L26:8`
+      `incompatible properties : ${prop}\n` +
+        `  definitions/Foo/properties/${prop}\n` +
+        `    at ${filePath}#L12:8\n` +
+        `  definitions/Foo2/properties/${prop}\n` +
+        `    at ${filePath}#L25:8`
     )
   }
 })
+
+// test("incompatible-properties-string-refobject", async () => {
+//   const diff = new OpenApiDiff({})
+//   const file = "src/test/specs/incompatible-properties/string-refobject.json"
+//   const filePath = fileUrl(path.resolve(file))
+
+//   try {
+//     await diff.compare(file, file)
+//     assert.fail("expected diff.compare() to throw")
+//   } catch (error) {
+//     const e = error as Error
+//     assert.equal(
+//       e.message,
+//       "incompatible properties : string-refobject\n" +
+//         "  definitions/Foo/properties/string-refobject\n" +
+//         `    at ${filePath}#L12:8\n` +
+//         "  definitions/Foo2/properties/string-refobject\n" +
+//         `    at ${filePath}#L25:8`
+//     )
+//   }
+// })
+
+// test("incompatible-properties-refstring-object", async () => {
+//   const diff = new OpenApiDiff({})
+//   const file = "src/test/specs/incompatible-properties/string-refobject.json"
+//   const filePath = fileUrl(path.resolve(file))
+
+//   try {
+//     await diff.compare(file, file)
+//     assert.fail("expected diff.compare() to throw")
+//   } catch (error) {
+//     const e = error as Error
+//     assert.equal(
+//       e.message,
+//       "incompatible properties : refstring-object\n" +
+//         "  definitions/Foo/properties/refstring-object\n" +
+//         `    at ${filePath}#L12:8\n` +
+//         "  definitions/Foo2/properties/refstring-object\n" +
+//         `    at ${filePath}#L25:8`
+//     )
+//   }
+// })
+
+// test("incompatible-properties-refstring-refobject", async () => {
+//   const diff = new OpenApiDiff({})
+//   const file = "src/test/specs/incompatible-properties/refstring-refobject.json"
+//   const filePath = fileUrl(path.resolve(file))
+
+//   try {
+//     await diff.compare(file, file)
+//     assert.fail("expected diff.compare() to throw")
+//   } catch (error) {
+//     const e = error as Error
+//     assert.equal(
+//       e.message,
+//       "incompatible properties : refstring-refobject\n" +
+//         "  definitions/Foo/properties/refstring-refobject\n" +
+//         `    at ${filePath}#L12:8\n` +
+//         "  definitions/Foo2/properties/refstring-refobject\n" +
+//         `    at ${filePath}#L25:8`
+//     )
+//   }
+// })
