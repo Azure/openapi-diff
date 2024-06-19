e9c9600f85ea85b4e64db6a0eb696f1813538aee
Konrad Jamrozik <kojamroz@microsoft.com>
2024-06-19 16:15:38 -0700

    Do not report `AddedRequiredProperty` if the property is `readOnly` (#336)

--- openapi-diff/src/modeler/AutoRest.Swagger/ComparisonMessages.cs
+++ openapi-diff/src/modeler/AutoRest.Swagger/ComparisonMessages.cs
@@ -14,7 +14,7 @@
         {
             Id = 1034,
             Code = nameof(ComparisonMessages.AddedRequiredProperty),
-            Message = "The new version has new required property '{0}' that was not found in the old version.",
+            Message = "The new version lists new non-read-only properties as required: '{0}'. These properties were not listed as required in the old version.",
             Type = MessageType.Addition
         };
 
